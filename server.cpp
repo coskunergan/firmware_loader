35202dce5f832c79beea904f2383579aca5b3fcf
coskunergan <coskun.ergan@gtmeu.com.tr>
2023-01-20 14:52:23 +0300

    V1.0.2
    -Forward result.

--- server.cpp
+++ server.cpp
@@ -29,6 +29,51 @@ bool file_exists(const fs::path &p, fs::file_status s = fs::file_status{})
 
 /**********************************************************************************************/
 
+int send_response(int fd, const char *header, const char *content_type, const char *body)
+{
+    const int max_response_size = 65536;
+    char response[max_response_size];
+
+    // Get current time for the HTTP header
+    time_t t1 = time(NULL);
+    struct tm *ltime = localtime(&t1);
+
+    // How many bytes in the body
+    int content_length = strlen(body);
+
+    int response_length = sprintf(response,
+                                  "%s\r\n"
+                                  "Access-Control-Allow-Origin: %s\r\n"
+                                  "Access-Control-Allow-Methods: %s\r\n"
+                                  "Content-Length: %d\r\n"
+                                  "Content-Type: %s\r\n"
+                                  "Date: %s" // asctime adds its own newline
+                                  "Connection: close\r\n"
+                                  "\r\n" // End of HTTP header
+                                  "%s",
+
+                                  header,
+                                  "*",
+                                  "POST, GET",
+                                  content_length,
+                                  content_type,
+                                  asctime(ltime),
+                                  body
+                                 );
+
+    // Send it all!
+    int rv = send(fd, response, response_length, 0);
+
+    if(rv < 0)
+    {
+        perror("send");
+    }
+
+    return rv;
+}
+
+/**********************************************************************************************/
+
 int main()
 {
     // winsock başlatma
@@ -73,8 +118,8 @@ int main()
 
     cout << "Sunucu dinlemede, 8080 portunda..." << endl;
 
-    char recvbuf[1024];
-    int recvbuflen = 1024;
+    char recvbuf[20];
+    int recvbuflen = 20;
     string myString{};
     SOCKET clientSocket;
 
@@ -92,6 +137,8 @@ int main()
         if(recv(clientSocket, recvbuf, recvbuflen, 0) > 0)
         {
             cout << "Sunucudan gelen veri: " << recvbuf << endl;
+            shutdown(listenSocket, SD_RECEIVE);
+            string response_str{};
             myString = recvbuf;
             size_t found = myString.find("FSW");
             if(found != string::npos)
@@ -113,57 +160,93 @@ int main()
                 else
                 {
                     cout << "File not deleted!!" << endl;
+                    response_str = "ERROR:5";
                 }
                 if(file_exists(repo))
                 {
                     cout << "Clone Repository done.." << endl;
-                    string str = "repos\\" + fsw_name + "\\" + fsw_name + ".tk";
+                    string str = "repos\\" + fsw_name + "\\" + fsw_name + ".tk";                    
                     const fs::path image{ str};
                     if(file_exists(image))
                     {
-                        ifstream sourceFile(str, std::ios::binary);
-                        ofstream destinationFile("BinaryFile.bin", std::ios::binary);
+                        ifstream sourceFile(str, ios::binary);
+                        ofstream destinationFile("BinaryFile.bin", ios::binary);
                         destinationFile << sourceFile.rdbuf();
                         sourceFile.close();
                         destinationFile.close();
-                        //system("LoaderStart.bat");
                         STARTUPINFO si;
                         PROCESS_INFORMATION pi;
-
                         ZeroMemory(&si, sizeof(si));
                         si.cb = sizeof(si);
                         ZeroMemory(&pi, sizeof(pi));
-
-                        if(CreateProcess(NULL, "LoaderStart.bat", NULL, NULL, FALSE, 0, NULL, NULL, &si, &pi))
+                        if(CreateProcess(NULL, (char *)"LoaderStart.bat", NULL, NULL, FALSE, 0, NULL, NULL, &si, &pi))
                         {
-                            std::cout << "Process started!" << std::endl;
+                            cout << "Process started!" << endl;
                             WaitForSingleObject(pi.hProcess, INFINITE);
                             CloseHandle(pi.hProcess);
                             CloseHandle(pi.hThread);
+                            response_str = "SUCCES";
+                            string fileContent, comparisonString;
+                            ifstream myfile("result.txt", ios::in);
+                            if(myfile.is_open())
+                            {
+                                getline(myfile, fileContent);
+                                myfile.close();
+                                if(fileContent == "okey")
+                                {
+                                    response_str = "SUCCES";
+                                }
+                                else if(fileContent == "fail")
+                                {
+                                    response_str = "ERROR:9";
+                                }
+                                else if(fileContent == "burn")
+                                {
+                                    response_str = "ERROR:8";
+                                }
+                                else if(fileContent == "tout")
+                                {
+                                    response_str = "ERROR:7";
+                                }
+                                else
+                                {
+                                    response_str = "ERROR:6";
+                                }
+                            }
+                            else
+                            {
+                                cout << "Unable to open file";
+                                response_str = "ERROR:5";
+                            }
                         }
                         else
                         {
-                            std::cout << "Failed to start process!" << std::endl;
+                            cout << "Failed to start process!" << endl;
+                            response_str = "ERROR:4";
                         }
                     }
                     else
                     {
                         cout << "Repository TK File Found!!" << endl;
+                        response_str = "ERROR:3";
                     }
                 }
                 else
                 {
                     cout << "Repository Not Found!!" << endl;
+                    response_str = "ERROR:2";
                 }
             }
             else
             {
                 cout << "Get the wrong FSW name!!" << endl;
-            }
+                response_str = "ERROR:1";
+            }            
+            send_response(clientSocket, "HTTP/1.1 200 OK", "text/plain", response_str.c_str());
+            fflush(stdout);
         }
     }
 
-    //closesocket(clientSocket);
     cout << "Soket kapatiliyor.." << endl;
     closesocket(listenSocket);
     WSACleanup();
